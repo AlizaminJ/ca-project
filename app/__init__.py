98b5db5d2ec4b3be6b41536d86b8eea68990805a
Dennis Knudsen <dkn@ambita.com>
2019-08-08 14:32:44 +0200

    unfucked linting

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from app import views, models
 
 # Instantiate our app and load configuration
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+
+from app import views, models
