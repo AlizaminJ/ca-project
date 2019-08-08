2dabc1e39bbecdaf7d4188170d48b3ac45c9e364
Dennis Knudsen <dkn@ambita.com>
2019-08-08 13:19:04 +0200

    lint stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+# from app import views, models
 
 # Instantiate our app and load configuration
 app = Flask(__name__)
@@ -7,4 +8,4 @@ app.config.from_object('config')
 
 db = SQLAlchemy(app)
 
-from app import views, models
+
